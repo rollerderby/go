26a462e74abb270d459eb66a23404f930af55973
Michael W. Mitton <mmitton@yssd.org>
2017-05-10 15:39:44 -0400

    Initial webserver

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,19 @@
 package main
 
-//go:generate $GOPATH/bin/esc -prefix html -o html.go ../../html
+//go:generate $GOPATH/bin/esc -o html.go ../../html
 
 import (
 	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"os/exec"
+	"os/signal"
+	"path"
 	"runtime"
-	"time"
 
+	"github.com/rollerderby/go/auth"
 	"github.com/rollerderby/go/entity"
 	"github.com/rollerderby/go/logger"
 	"github.com/rollerderby/go/ruleset"
@@ -16,37 +22,6 @@ import (
 )
 
 var log = logger.New("main")
-
-func addDummyTeams(names ...string) {
-	state.Root.Lock()
-	defer state.Root.Unlock()
-
-	for _, name := range names {
-		if t, err := entity.Teams.New(""); err != nil {
-			log.Errorf("Cannot create team: %v", err)
-		} else {
-			t.SetName(name)
-		}
-	}
-}
-
-func addDummyPerson() {
-	state.Root.Lock()
-	defer state.Root.Unlock()
-
-	per, err := entity.People.New("")
-	if err != nil {
-		log.Errorf("Cannot create person: %v", err)
-		return
-	}
-	per.SetName("Michael Mitton")
-	cert, err := per.Certs().New("")
-	if err != nil {
-		log.Errorf("Cannot create cert: %v", err)
-	}
-	cert.SetOrganization("WFTDA")
-	cert.SetType("NSO 2")
-}
 
 // openBrowser tries to open the URL in a browser,
 // and returns whether it succeed in doing so.
@@ -74,56 +49,93 @@ func setVersion() {
 	state.Root.Add("Version", "", versionStr)
 }
 
+func httpLog(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Debugf("%s: %s %s %s", r.Host, r.RemoteAddr, r.Method, r.URL)
+		w.Header().Set("cache-control", "private, max-age=0, no-cache")
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func extractHTMLFolder() error {
+	for key, value := range _escData {
+		if value.isDir && key != "/" {
+			local := path.Join(".", key)
+			if _, err := os.Stat(local); os.IsNotExist(err) {
+				log.Debugf("Create missing HTML directory: %v", key)
+				if err := os.MkdirAll(local, 0775); err != nil {
+					return err
+				}
+			}
+		}
+	}
+	for key, value := range _escData {
+		if !value.isDir {
+			local := path.Join(".", key)
+			if _, err := os.Stat(local); os.IsNotExist(err) {
+				log.Debugf("Create missing HTML file: %v", key)
+				if data, err := FSByte(false, key); err != nil {
+					return err
+				} else if err := ioutil.WriteFile(local, data, 0664); err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func initializeWebserver(port uint16, signals chan os.Signal) error {
+	if err := extractHTMLFolder(); err != nil {
+		log.Errorf("Cannot extract HTML folder: %v", err)
+		return err
+	}
+
+	httpMux := http.NewServeMux()
+	httpMux.Handle("/", http.FileServer(http.Dir("html")))
+	go func() {
+		log.Infof("Starting up http server on :%v", port)
+		log.Crit(http.ListenAndServe(fmt.Sprintf(":%d", port), httpLog(httpMux)))
+		signals <- os.Kill
+	}()
+
+	return nil
+}
+
 func main() {
 	log.Info("Initializing")
 
-	addTeams := flag.Bool("addTeams", false, "Add Dummy Teams")
-	addPerson := flag.Bool("addPerson", false, "Add Dummy Person")
+	port := flag.Int("port", 8000, "Port to listen on")
+	verbose := flag.Bool("v", false, "Print debugging information")
 	flag.Parse()
+
+	if *verbose {
+		logger.SetLevel(logger.DEBUG)
+	}
 
 	setVersion()
 
 	ruleset.Initialize()
 	entity.Initialize()
+	auth.Initialize()
 	websocket.Initialize()
-
-	// addDummyTeams("Team A", "Team B")
 
 	state.Root.LoadSavedConfigs()
 
-	if *addTeams {
-		teamsNeeded := map[string]bool{
-			"Team A": true,
-			"Team B": true,
-			"Team C": true,
-			"Team D": true,
-			"Team E": true,
-		}
-
-		for _, t := range entity.Teams.Values() {
-			delete(teamsNeeded, t.Name())
-		}
-
-		for key, _ := range teamsNeeded {
-			addDummyTeams(key)
-		}
+	if err := auth.AddGlobalUsers(); err != nil {
+		log.Errorf("Unable to add global admin user: %v", err)
+		os.Exit(1)
 	}
 
-	if *addPerson {
-		found := false
-		for _, p := range entity.People.Values() {
-			if p.Name() == "Michael Mitton" {
-				found = true
-				break
-			}
-		}
-		if !found {
-			addDummyPerson()
-		}
+	signals := make(chan os.Signal, 1)
+	if err := initializeWebserver(uint16(*port), signals); err != nil {
+		return
 	}
-
 	go state.Root.SaveLoop()
 
 	openBrowser("http://localhost:8000")
-	time.Sleep(15 * time.Second)
+
+	signal.Notify(signals, os.Interrupt, os.Kill)
+	s := <-signals
+	log.Noticef("Server received signal: %v.  Shutting down", s)
 }
